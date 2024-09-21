bc0b4efea298888684bab67c2e0c91ca2acf9506
SriomAryan <sriomaryanm@gmail.com>
2024-09-22 04:43:39 +0530

    updating

---
+++ Hello World 2.py
@@ -0,0 +1,13 @@
+#print("Hello!","World")
+"nfjsafsvsjgbhggvsggvsbg"
+x=5
+if(x==5):
+    print("Hello")
+    print("World")
+
+x=5
+if(x==5):
+    print("Hello")
+else:
+    print("GEC Vaishali")
+
---
+++ Python 21.09.2024.py
@@ -0,0 +1,131 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import numpy
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    import numpy
+ModuleNotFoundError: No module named 'numpy'
+>>> 2+6
+8
+>>> 2**3
+8
+>>> "SAM"*3
+'SAMSAMSAM'
+>>> "SAM "*5
+'SAM SAM SAM SAM SAM '
+>>> x=5
+>>> type(x)
+<class 'int'>
+>>> x="GECV"
+>>> type(x)
+<class 'str'>
+>>> 
+============================================= RESTART: C:/Users/HP/Desktop/PythonGitHub/Python-Workshop/Projects/Hello World.py ============================================
+Hello
+World
+>>> x=input("Enter a number: ")
+Enter a number: 9
+>>> print(x)
+9
+>>> x=input("Enter two number: ")
+Enter two number: 4
+>>> type(x)
+<class 'str'>
+>>> x=int(input("Enter a number: "))
+Enter a number: 5
+>>> type(x)
+<class 'int'>
+>>> x=float(input("Enter a number: "))
+Enter a number: 3.4
+>>> type(x)
+<class 'float'>
+>>> x,y=input("Enter Two Number: ").split()
+Enter Two Number: 4 5
+>>> type(x)
+<class 'str'>
+>>> type(y)
+<class 'str'>
+>>> x=int(x)
+>>> y=int(y)
+>>> x+y
+9
+>>> 
+============================================= RESTART: C:\Users\HP\Desktop\PythonGitHub\Python-Workshop\Projects\Hello World.py ============================================
+Hello World
+>>> 
+============================================= RESTART: C:\Users\HP\Desktop\PythonGitHub\Python-Workshop\Projects\Hello World.py ============================================
+Hello! World
+4//2
+2
+x**=2
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    x**=2
+NameError: name 'x' is not defined
+x
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    x
+NameError: name 'x' is not defined
+x = ["Rose", "Lotus"]
+print("Rose" in x)
+True
+print("Rose" not in x)
+False
+x=5
+x**=2
+x
+25
+x=6
+y=7
+print(x ix y)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print(x is y)
+False
+y=x
+print(x is y)
+True
+# hash is use for single line comment
+# 2nd comment
+# 3rd comment
+
+============================================= RESTART: C:\Users\HP\Desktop\PythonGitHub\Python-Workshop\Projects\Hello World.py ============================================
+Hello! World
+if(x==5)
+SyntaxError: expected ':'
+x=5
+if(x==5):
+    print("Hello")
+    
+SyntaxError: multiple statements found while compiling a single statement
+x=5
+
+============================================= RESTART: C:\Users\HP\Desktop\PythonGitHub\Python-Workshop\Projects\Hello World.py ============================================
+Hello! World
+Hello
+GEC Vaishali
+
+============================================= RESTART: C:\Users\HP\Desktop\PythonGitHub\Python-Workshop\Projects\Hello World.py ============================================
+Hello! World
+GEC Vaishali
+
+============================================= RESTART: C:\Users\HP\Desktop\PythonGitHub\Python-Workshop\Projects\Hello World.py ============================================
+Hello! World
+Hello
+World
+
+============================================= RESTART: C:\Users\HP\Desktop\PythonGitHub\Python-Workshop\Projects\Hello World.py ============================================
+Hello! World
+Hello
+World
+GEC Vaishali
+
+============================================= RESTART: C:\Users\HP\Desktop\PythonGitHub\Python-Workshop\Projects\Hello World.py ============================================
+Hello
+World
+GEC Vaishali
+
+============================================= RESTART: C:\Users\HP\Desktop\PythonGitHub\Python-Workshop\Projects\Hello World.py ============================================
+Hello
+World
+GEC Vaishali
